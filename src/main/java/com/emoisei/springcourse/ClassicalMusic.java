b4e6878001ccbed708bca72352acb6a308f3c795
Egor <e.moisei1@gmail.com>
2024-01-19 00:14:18 +0300

    add annotation

--- src/main/java/com/emoisei/springcourse/ClassicalMusic.java
+++ src/main/java/com/emoisei/springcourse/ClassicalMusic.java
@@ -1,14 +1,23 @@
 package com.emoisei.springcourse;
 
+import org.springframework.stereotype.Component;
+
+@Component
 public class ClassicalMusic implements Music {
+//    private ClassicalMusic(){
+//    }
+//
+//    public static ClassicalMusic getClassicalMusic() {
+//        return new ClassicalMusic();
+//    }
 
-    public void doMyInit(){
-        System.out.println("Doing my initialization");
-    }
-
-    public void doMyDestroy(){
-        System.out.println("Doing my destruction");
-    }
+//    public void doMyInit(){
+//        System.out.println("Doing my initialization");
+//    }
+//
+//    public void doMyDestroy(){
+//        System.out.println("Doing my destruction");
+//    }
 
 
 
